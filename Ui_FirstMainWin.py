bec6231e9effe0e729ae54b0c752dab12a65f0fe
honqiang.lee <honqiang.lee@gmail.com>
2019-05-17 20:41:28 +0800

    postgresql UI

--- Ui_FirstMainWin.py
+++ Ui_FirstMainWin.py
@@ -11,25 +11,18 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(899, 670)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(0, 500, 111, 41))
-        self.pushButton.setObjectName("pushButton")
         self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
-        self.tableWidget.setGeometry(QtCore.QRect(0, 0, 791, 491))
+        self.tableWidget.setGeometry(QtCore.QRect(0, 0, 891, 491))
+        self.tableWidget.setRowCount(5)
+        self.tableWidget.setColumnCount(14)
         self.tableWidget.setObjectName("tableWidget")
-        self.tableWidget.setColumnCount(0)
-        self.tableWidget.setRowCount(0)
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(240, 520, 93, 28))
-        self.pushButton_2.setObjectName("pushButton_2")
+        self.btn_load = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_load.setGeometry(QtCore.QRect(400, 540, 93, 28))
+        self.btn_load.setObjectName("btn_load")
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
@@ -40,8 +33,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "PushButton"))
-        self.pushButton_2.setText(_translate("MainWindow", "PushButton"))
+        self.btn_load.setText(_translate("MainWindow", "load"))
 
 
 if __name__ == "__main__":
